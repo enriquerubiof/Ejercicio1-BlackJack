2ed546188c3dd8e8e6d7c316a31d196ae361c71b
enriq <enriq@LAPTOP-10A50H35>
2023-07-20 07:42:26 +0200

    volteo cartas

--- src/gui/MesaBlackJack.java
+++ src/gui/MesaBlackJack.java
@@ -17,32 +17,42 @@ import java.awt.Component;
 import javax.swing.border.LineBorder;
 import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.Label;
+
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
+import java.awt.Window.Type;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 public class MesaBlackJack extends JFrame {
 
 	private JPanel contentPane;
-	private JLabel lblTuCarta2;
-	private JLabel lblTuCarta2_2;
-	private JLabel lblTuCarta2_3;
-	private JLabel lblTuCarta2_4;
-	private JLabel lblTuCarta2_5;
-	private JLabel lblTuCarta2_6;
-	private JLabel lblTuCarta2_7;
-	private JLabel lblTuCarta2_8;
-	private JLabel lblTuCarta2_9;
-	private JLabel lblTuCarta2_10;
-	private JLabel lblTuCarta2_11;
-	private JLabel lblTuCarta2_12;
-	private JLabel lblTuCarta2_13;
-	private JLabel lblTuCarta2_14;
 	private JLabel lblTuCarta1;
-	private JLabel lblTuCarta2_1;
-	private JLabel lblTuCarta2_15;
-	private JButton btnNewButton;
-	private JButton btnNewButton_1;
-
+	private JLabel lblTuCarta3;
+	private JLabel lblRvCarta2;
+	private JLabel lblRvCarta3;
+	private JLabel lblTuCarta4;
+	private JLabel lblRvCarta4;
+	private JLabel lblTuCarta5;
+	private JLabel lblRvCarta5;
+	private JLabel lblTuCarta6;
+	private JLabel lblRvCarta6;
+	private JLabel lblTuCarta7;
+	private JLabel lblRvCarta7;
+	private JLabel lblTuCarta8;
+	private JLabel lblRvCarta8;
+	private JLabel lblRvCarta1;
+	private JLabel lblTuCarta2;
+	private JLabel lblBaraja;
+	private JButton btnComenzar;
+	private JButton btnPasar;
+	
+	private int carta;
+	private ArrayList<JLabel> cartasPuls;
 	/**
 	 * Launch the application.
 	 */
@@ -65,6 +75,9 @@ public class MesaBlackJack extends JFrame {
 	 * Create the frame.
 	 */
 	public MesaBlackJack() {
+		carta = 0;
+		cartasPuls = new ArrayList<JLabel>();
+		setType(Type.POPUP);
 		setResizable(false);
 		setIgnoreRepaint(true);
 		this.setExtendedState(Frame.MAXIMIZED_BOTH);
@@ -82,154 +95,220 @@ public class MesaBlackJack extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(new MigLayout("", "[25.00][150][125px][][][][][][][][][][][][][][][][]", "[grow][210][39.00][210][grow]"));
 		
+		lblTuCarta1 = new JLabel("");
+		lblTuCarta1.addMouseListener(new MouseAdapter()
+		{
+			public void mouseClicked(MouseEvent e)
+			{
+				if (carta == 1)
+					voltearCarta(e);
+			}
+		});
+		lblTuCarta1.setIcon(null);
+		lblTuCarta1.setSize(new Dimension(125, 182));
+		lblTuCarta1.setPreferredSize(new Dimension(125, 182));
+		lblTuCarta1.setHorizontalTextPosition(SwingConstants.LEADING);
+		lblTuCarta1.setBorder(new LineBorder(new Color(255, 255, 255)));
+		lblTuCarta1.setAlignmentY(0.0f);
+		contentPane.add(lblTuCarta1, "cell 2 1");
+		
 		lblTuCarta2 = new JLabel("");
+		lblTuCarta2.addMouseListener(new MouseAdapter()
+		{
+			@Override
+			public void mouseClicked(MouseEvent e)
+			{
+				if (carta == 2)
+				voltearCarta(e);
+			}
+		});
 		lblTuCarta2.setIcon(null);
 		lblTuCarta2.setSize(new Dimension(125, 182));
 		lblTuCarta2.setPreferredSize(new Dimension(125, 182));
 		lblTuCarta2.setHorizontalTextPosition(SwingConstants.LEADING);
 		lblTuCarta2.setBorder(new LineBorder(new Color(255, 255, 255)));
 		lblTuCarta2.setAlignmentY(0.0f);
-		contentPane.add(lblTuCarta2, "cell 2 1");
+		contentPane.add(lblTuCarta2, "cell 4 1");
 		
-		lblTuCarta2_1 = new JLabel("");
-		lblTuCarta2_1.setIcon(null);
-		lblTuCarta2_1.setSize(new Dimension(125, 182));
-		lblTuCarta2_1.setPreferredSize(new Dimension(125, 182));
-		lblTuCarta2_1.setHorizontalTextPosition(SwingConstants.LEADING);
-		lblTuCarta2_1.setBorder(new LineBorder(new Color(255, 255, 255)));
-		lblTuCarta2_1.setAlignmentY(0.0f);
-		contentPane.add(lblTuCarta2_1, "cell 4 1");
+		lblTuCarta3 = new JLabel("");
+		lblTuCarta3.addMouseListener(new MouseAdapter()
+		{
+			public void mouseClicked(MouseEvent e)
+			{
+					if (carta == 3)
+					voltearCarta(e);
+			}
+		});
+		lblTuCarta3.setSize(new Dimension(125, 182));
+		lblTuCarta3.setPreferredSize(new Dimension(125, 182));
+		lblTuCarta3.setHorizontalTextPosition(SwingConstants.LEADING);
+		lblTuCarta3.setBorder(new LineBorder(new Color(255, 255, 255)));
+		lblTuCarta3.setAlignmentY(0.0f);
+		contentPane.add(lblTuCarta3, "cell 6 1");
 		
-		lblTuCarta2_2 = new JLabel("");
-		lblTuCarta2_2.setSize(new Dimension(125, 182));
-		lblTuCarta2_2.setPreferredSize(new Dimension(125, 182));
-		lblTuCarta2_2.setHorizontalTextPosition(SwingConstants.LEADING);
-		lblTuCarta2_2.setBorder(new LineBorder(new Color(255, 255, 255)));
-		lblTuCarta2_2.setAlignmentY(0.0f);
-		contentPane.add(lblTuCarta2_2, "cell 6 1");
+		lblTuCarta4 = new JLabel("");
+		lblTuCarta4.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e)
+			{
+				if (carta == 4)
+				voltearCarta(e);
+			}
+		});
+		lblTuCarta4.setSize(new Dimension(125, 182));
+		lblTuCarta4.setPreferredSize(new Dimension(125, 182));
+		lblTuCarta4.setHorizontalTextPosition(SwingConstants.LEADING);
+		lblTuCarta4.setBorder(new LineBorder(new Color(255, 255, 255)));
+		lblTuCarta4.setAlignmentY(0.0f);
+		contentPane.add(lblTuCarta4, "cell 8 1");
 		
-		lblTuCarta2_5 = new JLabel("");
-		lblTuCarta2_5.setSize(new Dimension(125, 182));
-		lblTuCarta2_5.setPreferredSize(new Dimension(125, 182));
-		lblTuCarta2_5.setHorizontalTextPosition(SwingConstants.LEADING);
-		lblTuCarta2_5.setBorder(new LineBorder(new Color(255, 255, 255)));
-		lblTuCarta2_5.setAlignmentY(0.0f);
-		contentPane.add(lblTuCarta2_5, "cell 8 1");
+		lblTuCarta5 = new JLabel("");
+		lblTuCarta5.setSize(new Dimension(125, 182));
+		lblTuCarta5.setPreferredSize(new Dimension(125, 182));
+		lblTuCarta5.setHorizontalTextPosition(SwingConstants.LEADING);
+		lblTuCarta5.setBorder(new LineBorder(new Color(255, 255, 255)));
+		lblTuCarta5.setAlignmentY(0.0f);
+		contentPane.add(lblTuCarta5, "cell 10 1");
 		
-		lblTuCarta2_7 = new JLabel("");
-		lblTuCarta2_7.setSize(new Dimension(125, 182));
-		lblTuCarta2_7.setPreferredSize(new Dimension(125, 182));
-		lblTuCarta2_7.setHorizontalTextPosition(SwingConstants.LEADING);
-		lblTuCarta2_7.setBorder(new LineBorder(new Color(255, 255, 255)));
-		lblTuCarta2_7.setAlignmentY(0.0f);
-		contentPane.add(lblTuCarta2_7, "cell 10 1");
+		lblTuCarta6 = new JLabel("");
+		lblTuCarta6.setSize(new Dimension(125, 182));
+		lblTuCarta6.setPreferredSize(new Dimension(125, 182));
+		lblTuCarta6.setHorizontalTextPosition(SwingConstants.LEADING);
+		lblTuCarta6.setBorder(new LineBorder(new Color(255, 255, 255)));
+		lblTuCarta6.setAlignmentY(0.0f);
+		contentPane.add(lblTuCarta6, "cell 12 1");
 		
-		lblTuCarta2_9 = new JLabel("");
-		lblTuCarta2_9.setSize(new Dimension(125, 182));
-		lblTuCarta2_9.setPreferredSize(new Dimension(125, 182));
-		lblTuCarta2_9.setHorizontalTextPosition(SwingConstants.LEADING);
-		lblTuCarta2_9.setBorder(new LineBorder(new Color(255, 255, 255)));
-		lblTuCarta2_9.setAlignmentY(0.0f);
-		contentPane.add(lblTuCarta2_9, "cell 12 1");
+		lblTuCarta7 = new JLabel("");
+		lblTuCarta7.setSize(new Dimension(125, 182));
+		lblTuCarta7.setPreferredSize(new Dimension(125, 182));
+		lblTuCarta7.setHorizontalTextPosition(SwingConstants.LEADING);
+		lblTuCarta7.setBorder(new LineBorder(new Color(255, 255, 255)));
+		lblTuCarta7.setAlignmentY(0.0f);
+		contentPane.add(lblTuCarta7, "cell 14 1");
 		
-		lblTuCarta2_11 = new JLabel("");
-		lblTuCarta2_11.setSize(new Dimension(125, 182));
-		lblTuCarta2_11.setPreferredSize(new Dimension(125, 182));
-		lblTuCarta2_11.setHorizontalTextPosition(SwingConstants.LEADING);
-		lblTuCarta2_11.setBorder(new LineBorder(new Color(255, 255, 255)));
-		lblTuCarta2_11.setAlignmentY(0.0f);
-		contentPane.add(lblTuCarta2_11, "cell 14 1");
+		lblTuCarta8 = new JLabel("");
+		lblTuCarta8.setSize(new Dimension(125, 182));
+		lblTuCarta8.setPreferredSize(new Dimension(125, 182));
+		lblTuCarta8.setHorizontalTextPosition(SwingConstants.LEADING);
+		lblTuCarta8.setBorder(new LineBorder(new Color(255, 255, 255)));
+		lblTuCarta8.setAlignmentY(0.0f);
+		contentPane.add(lblTuCarta8, "cell 17 1");
 		
-		lblTuCarta2_13 = new JLabel("");
-		lblTuCarta2_13.setSize(new Dimension(125, 182));
-		lblTuCarta2_13.setPreferredSize(new Dimension(125, 182));
-		lblTuCarta2_13.setHorizontalTextPosition(SwingConstants.LEADING);
-		lblTuCarta2_13.setBorder(new LineBorder(new Color(255, 255, 255)));
-		lblTuCarta2_13.setAlignmentY(0.0f);
-		contentPane.add(lblTuCarta2_13, "cell 17 1");
+		lblBaraja = new JLabel("");
+		lblBaraja.setIcon(new ImageIcon(MesaBlackJack.class.getResource("/imagenes/reverso5.jpg")));
+		lblBaraja.setSize(new Dimension(125, 182));
+		lblBaraja.setPreferredSize(new Dimension(125, 182));
+		lblBaraja.setHorizontalTextPosition(SwingConstants.LEADING);
+		lblBaraja.setBorder(new LineBorder(new Color(255, 255, 255)));
+		lblBaraja.setAlignmentY(0.0f);
+		contentPane.add(lblBaraja, "cell 1 1 1 3");
 		
-		lblTuCarta2_15 = new JLabel("");
-		lblTuCarta2_15.setIcon(new ImageIcon(MesaBlackJack.class.getResource("/imagenes/reverso5.jpg")));
-		lblTuCarta2_15.setSize(new Dimension(125, 182));
-		lblTuCarta2_15.setPreferredSize(new Dimension(125, 182));
-		lblTuCarta2_15.setHorizontalTextPosition(SwingConstants.LEADING);
-		lblTuCarta2_15.setBorder(new LineBorder(new Color(255, 255, 255)));
-		lblTuCarta2_15.setAlignmentY(0.0f);
-		contentPane.add(lblTuCarta2_15, "cell 1 1 1 3");
+		btnComenzar = new JButton("Comenzar");
+		btnComenzar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				activarTablero();
+			}
+		});
+		contentPane.add(btnComenzar, "cell 6 2 3 1,alignx center");
 		
-		btnNewButton = new JButton("Comenzar");
-		contentPane.add(btnNewButton, "cell 6 2 3 1,alignx center");
+		btnPasar = new JButton("Pasar");
+		btnPasar.setEnabled(false);
+		btnPasar.setAlignmentY(Component.TOP_ALIGNMENT);
+		contentPane.add(btnPasar, "cell 10 2 3 1,alignx center");
 		
-		btnNewButton_1 = new JButton("Pasar");
-		contentPane.add(btnNewButton_1, "cell 10 2 3 1,alignx center");
+		lblRvCarta1 = new JLabel("");
+		lblRvCarta1.setVerticalTextPosition(SwingConstants.TOP);
+		lblRvCarta1.setSize(new Dimension(182, 125));
+		lblRvCarta1.setPreferredSize(new Dimension(125, 182));
+		lblRvCarta1.setOpaque(true);
+		lblRvCarta1.setHorizontalTextPosition(SwingConstants.LEADING);
+		lblRvCarta1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblRvCarta1.setBorder(new LineBorder(new Color(255, 255, 255), 1, true));
+		lblRvCarta1.setBackground(new Color(0, 128, 64));
+		lblRvCarta1.setAlignmentY(0.0f);
+		contentPane.add(lblRvCarta1, "cell 2 3");
 		
-		lblTuCarta1 = new JLabel("");
-		lblTuCarta1.setVerticalTextPosition(SwingConstants.TOP);
-		lblTuCarta1.setSize(new Dimension(182, 125));
-		lblTuCarta1.setPreferredSize(new Dimension(125, 182));
-		lblTuCarta1.setOpaque(true);
-		lblTuCarta1.setHorizontalTextPosition(SwingConstants.LEADING);
-		lblTuCarta1.setHorizontalAlignment(SwingConstants.LEFT);
-		lblTuCarta1.setBorder(new LineBorder(new Color(255, 255, 255), 1, true));
-		lblTuCarta1.setBackground(new Color(0, 128, 64));
-		lblTuCarta1.setAlignmentY(0.0f);
-		contentPane.add(lblTuCarta1, "cell 2 3");
+		lblRvCarta2 = new JLabel("");
+		lblRvCarta2.setSize(new Dimension(125, 182));
+		lblRvCarta2.setPreferredSize(new Dimension(125, 182));
+		lblRvCarta2.setHorizontalTextPosition(SwingConstants.LEADING);
+		lblRvCarta2.setBorder(new LineBorder(new Color(255, 255, 255)));
+		lblRvCarta2.setAlignmentY(0.0f);
+		contentPane.add(lblRvCarta2, "cell 4 3");
 		
-		lblTuCarta2_3 = new JLabel("");
-		lblTuCarta2_3.setSize(new Dimension(125, 182));
-		lblTuCarta2_3.setPreferredSize(new Dimension(125, 182));
-		lblTuCarta2_3.setHorizontalTextPosition(SwingConstants.LEADING);
-		lblTuCarta2_3.setBorder(new LineBorder(new Color(255, 255, 255)));
-		lblTuCarta2_3.setAlignmentY(0.0f);
-		contentPane.add(lblTuCarta2_3, "cell 4 3");
+		lblRvCarta3 = new JLabel("");
+		lblRvCarta3.setSize(new Dimension(125, 182));
+		lblRvCarta3.setPreferredSize(new Dimension(125, 182));
+		lblRvCarta3.setHorizontalTextPosition(SwingConstants.LEADING);
+		lblRvCarta3.setBorder(new LineBorder(new Color(255, 255, 255)));
+		lblRvCarta3.setAlignmentY(0.0f);
+		contentPane.add(lblRvCarta3, "cell 6 3");
 		
-		lblTuCarta2_4 = new JLabel("");
-		lblTuCarta2_4.setSize(new Dimension(125, 182));
-		lblTuCarta2_4.setPreferredSize(new Dimension(125, 182));
-		lblTuCarta2_4.setHorizontalTextPosition(SwingConstants.LEADING);
-		lblTuCarta2_4.setBorder(new LineBorder(new Color(255, 255, 255)));
-		lblTuCarta2_4.setAlignmentY(0.0f);
-		contentPane.add(lblTuCarta2_4, "cell 6 3");
+		lblRvCarta4 = new JLabel("");
+		lblRvCarta4.setSize(new Dimension(125, 182));
+		lblRvCarta4.setPreferredSize(new Dimension(125, 182));
+		lblRvCarta4.setHorizontalTextPosition(SwingConstants.LEADING);
+		lblRvCarta4.setBorder(new LineBorder(new Color(255, 255, 255)));
+		lblRvCarta4.setAlignmentY(0.0f);
+		contentPane.add(lblRvCarta4, "cell 8 3");
 		
-		lblTuCarta2_6 = new JLabel("");
-		lblTuCarta2_6.setSize(new Dimension(125, 182));
-		lblTuCarta2_6.setPreferredSize(new Dimension(125, 182));
-		lblTuCarta2_6.setHorizontalTextPosition(SwingConstants.LEADING);
-		lblTuCarta2_6.setBorder(new LineBorder(new Color(255, 255, 255)));
-		lblTuCarta2_6.setAlignmentY(0.0f);
-		contentPane.add(lblTuCarta2_6, "cell 8 3");
+		lblRvCarta5 = new JLabel("");
+		lblRvCarta5.setSize(new Dimension(125, 182));
+		lblRvCarta5.setPreferredSize(new Dimension(125, 182));
+		lblRvCarta5.setHorizontalTextPosition(SwingConstants.LEADING);
+		lblRvCarta5.setBorder(new LineBorder(new Color(255, 255, 255)));
+		lblRvCarta5.setAlignmentY(0.0f);
+		contentPane.add(lblRvCarta5, "cell 10 3");
 		
-		lblTuCarta2_8 = new JLabel("");
-		lblTuCarta2_8.setSize(new Dimension(125, 182));
-		lblTuCarta2_8.setPreferredSize(new Dimension(125, 182));
-		lblTuCarta2_8.setHorizontalTextPosition(SwingConstants.LEADING);
-		lblTuCarta2_8.setBorder(new LineBorder(new Color(255, 255, 255)));
-		lblTuCarta2_8.setAlignmentY(0.0f);
-		contentPane.add(lblTuCarta2_8, "cell 10 3");
+		lblRvCarta6 = new JLabel("");
+		lblRvCarta6.setSize(new Dimension(125, 182));
+		lblRvCarta6.setPreferredSize(new Dimension(125, 182));
+		lblRvCarta6.setHorizontalTextPosition(SwingConstants.LEADING);
+		lblRvCarta6.setBorder(new LineBorder(new Color(255, 255, 255)));
+		lblRvCarta6.setAlignmentY(0.0f);
+		contentPane.add(lblRvCarta6, "cell 12 3");
 		
-		lblTuCarta2_10 = new JLabel("");
-		lblTuCarta2_10.setSize(new Dimension(125, 182));
-		lblTuCarta2_10.setPreferredSize(new Dimension(125, 182));
-		lblTuCarta2_10.setHorizontalTextPosition(SwingConstants.LEADING);
-		lblTuCarta2_10.setBorder(new LineBorder(new Color(255, 255, 255)));
-		lblTuCarta2_10.setAlignmentY(0.0f);
-		contentPane.add(lblTuCarta2_10, "cell 12 3");
+		lblRvCarta7 = new JLabel("");
+		lblRvCarta7.setSize(new Dimension(125, 182));
+		lblRvCarta7.setPreferredSize(new Dimension(125, 182));
+		lblRvCarta7.setHorizontalTextPosition(SwingConstants.LEADING);
+		lblRvCarta7.setBorder(new LineBorder(new Color(255, 255, 255)));
+		lblRvCarta7.setAlignmentY(0.0f);
+		contentPane.add(lblRvCarta7, "cell 14 3");
 		
-		lblTuCarta2_12 = new JLabel("");
-		lblTuCarta2_12.setSize(new Dimension(125, 182));
-		lblTuCarta2_12.setPreferredSize(new Dimension(125, 182));
-		lblTuCarta2_12.setHorizontalTextPosition(SwingConstants.LEADING);
-		lblTuCarta2_12.setBorder(new LineBorder(new Color(255, 255, 255)));
-		lblTuCarta2_12.setAlignmentY(0.0f);
-		contentPane.add(lblTuCarta2_12, "cell 14 3");
-		
-		lblTuCarta2_14 = new JLabel("");
-		lblTuCarta2_14.setSize(new Dimension(125, 182));
-		lblTuCarta2_14.setPreferredSize(new Dimension(125, 182));
-		lblTuCarta2_14.setHorizontalTextPosition(SwingConstants.LEADING);
-		lblTuCarta2_14.setBorder(new LineBorder(new Color(255, 255, 255)));
-		lblTuCarta2_14.setAlignmentY(0.0f);
-		contentPane.add(lblTuCarta2_14, "cell 17 3");
+		lblRvCarta8 = new JLabel("");
+		lblRvCarta8.setSize(new Dimension(125, 182));
+		lblRvCarta8.setPreferredSize(new Dimension(125, 182));
+		lblRvCarta8.setHorizontalTextPosition(SwingConstants.LEADING);
+		lblRvCarta8.setBorder(new LineBorder(new Color(255, 255, 255)));
+		lblRvCarta8.setAlignmentY(0.0f);
+		contentPane.add(lblRvCarta8, "cell 17 3");
+	}
+
+	protected void voltearCarta(MouseEvent e)
+	{
+		if (carta == 1)
+		{
+			cartasPuls.add(this.lblTuCarta2);
+			cartasPuls.add(this.lblTuCarta3);
+			cartasPuls.add(this.lblTuCarta4);
+			cartasPuls.add(this.lblTuCarta5);
+			cartasPuls.add(this.lblTuCarta6);
+			cartasPuls.add(this.lblTuCarta7);
+			cartasPuls.add(this.lblTuCarta8);
+		}
+		((JLabel)e.getComponent()).setIcon(new ImageIcon(MesaBlackJack.class.getResource("/imagenes/cartasPoker/10_of_clubs.png")));
+		cartasPuls.get(carta-1).setIcon(new ImageIcon(MesaBlackJack.class.getResource("/imagenes/reverso5.jpg")));
+		carta++;
+	}
+
+	protected void activarTablero()
+	{
+		this.btnComenzar.setEnabled(false);
+		this.btnPasar.setEnabled(true);
+		this.lblTuCarta1.setIcon(new ImageIcon(MesaBlackJack.class.getResource("/imagenes/reverso5.jpg")));
+		carta++;
 	}
 
 }
